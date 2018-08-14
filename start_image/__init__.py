f9e60204729de96257a8d7e615bbbbed7479f010
ChrisTimperley <christimperley@googlemail.com>
2018-08-14 15:23:30 -0400

    added basic stub methods to Python module

--- start_image/__init__.py
+++ start_image/__init__.py
@@ -1,3 +1,16 @@
 """
 This module is responsible for managing the Docker images used by START.
 """
+import os
+
+DIR_DOCKER = os.path.join(os.path.dirname(__file__), '../docker')
+DOCKERFILE_BASE = os.path.join(DIR_DOCKER, 'Dockerfile')
+DOCKERFILE_SCENARIO = os.path.join(DIR_DOCKER, 'Dockerfile.scenario')
+
+
+def build_base_image():  # type: () -> None
+    raise NotImplementedError
+
+
+def build_scenario_image():  # type: () -> None
+    raise NotImplementedError
