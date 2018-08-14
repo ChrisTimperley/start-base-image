539be315720c9d90f592561b6732ca1ee13730fb
ChrisTimperley <christimperley@googlemail.com>
2018-08-14 18:17:31 -0400

    fixed inclusion of Dockerfile

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     install_requires=['docker'],
     include_package_data=True,
     packages=['start_image'],
-    data_files=[('docker', ['Dockerfile'])],
+    package_data={'': ['Dockerfile']},
     entry_points = {
         'console_scripts': [ 'start-image = start_image.cli:main' ]
     }
