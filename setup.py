c9d4aa6c483effd5561b0afab133441fd57cd1e4
ChrisTimperley <christimperley@googlemail.com>
2018-08-16 10:34:09 -0400

    removed CLI entrypoint

--- setup.py
+++ setup.py
@@ -11,8 +11,5 @@ setuptools.setup(
     install_requires=['docker'],
     include_package_data=True,
     packages=['start_image'],
-    package_data={'': ['Dockerfile', 'Dockerfile.scenario']},
-    entry_points = {
-        'console_scripts': [ 'start-image = start_image.cli:main' ]
-    }
+    package_data={'': ['Dockerfile', 'Dockerfile.scenario']}
 )
