37b39ed16a3ffc3167f60f1ccadffcba91d1c137
ChrisTimperley <christimperley@googlemail.com>
2018-08-17 14:53:29 -0400

    install mission.txt to Docker image

--- start_image/build.py
+++ start_image/build.py
@@ -60,9 +60,12 @@ def build_scenario_image(scenario):  # type: (Scenario) -> None
         dockerfile = os.path.join(dir_build, 'Dockerfile')
         diff_fn = os.path.join(dir_build, 'vulnerability.diff')
         cfg_fn = os.path.join(dir_build, 'scenario.config')
+        mission_fn = os.path.join(dir_build, 'mission.txt')
         shutil.copyfile(DOCKERFILE_SCENARIO, dockerfile)
         shutil.copyfile(scenario.diff_fn, diff_fn)
         shutil.copyfile(scenario.filename, cfg_fn)
+        # FIXME scenario config assumes mission is stored in mission.txt
+        shutil.copyfile(scenario.mission.filename, mission_fn)
         dkr.images.prune()
         dkr.images.build(path=dir_build,
                          tag=image_name(scenario),
