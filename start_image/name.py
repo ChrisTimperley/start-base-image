24f316c7bb37fed8df903a5881b8344e2b4d663a
ChrisTimperley <christimperley@googlemail.com>
2018-09-03 12:56:01 -0400

    added repo_and_tag method

--- start_image/name.py
+++ start_image/name.py
@@ -2,9 +2,18 @@
 This module is responsible for assigning names to the Docker images for
 START scenarios.
 """
+from typing import Tuple
+
 from start_core.scenario import Scenario
 
 BASE_IMAGE_NAME = "christimperley/start:base"
+
+
+def repo_and_tag(scenario):  # type: (Scenario) -> Tuple[str, str]
+    # FIXME ensure tag is legal!
+    repo = "christimperley/start"
+    tag = scenario.name
+    return (repo, tag)
 
 
 def name(scenario):  # type: (Scenario) -> str
@@ -12,5 +21,4 @@ def name(scenario):  # type: (Scenario) -> str
     Returns the fully qualified name of the Docker image for a scenario,
     given by its name.
     """
-    # FIXME ensure name is legal
-    return "christimperley/start:{}".format(scenario.name)
+    return ':'.join(repo_and_tag(scenario))
